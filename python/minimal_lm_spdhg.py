3481263d80301f46d4fe619ce3d5c787f6d927d4
Georg Schramm <georg.schramm@posteo.net>
2021-05-01 16:17:30 +0200

    correct pre-conditioned step sizes in LM SPDHG

--- python/minimal_lm_spdhg.py
+++ python/minimal_lm_spdhg.py
@@ -102,8 +102,9 @@ def SPDHG_LM(sens, events, mu, contam_list,
 
   # set step sizes
   if precond:
-    S = gamma*rho/sens
+    S = [gamma*rho/fwd_lm(1,sens,events[slice(x,None,2)]) for x in [0,1]]
     T = p_p*(rho/(gamma*sens)).min()
+    #T = p_p*np.array([rho/(gamma*back_lm(1,sens,events[slice(x,None,2)])) for x in [0,1]]).min(0)
   else:
     S = [gamma*rho/sens[0], gamma*rho/sens[1]]
     T = p_p*min(rho/(gamma*sens[0]),rho/(gamma*sens[1]))
@@ -121,15 +122,16 @@ def SPDHG_LM(sens, events, mu, contam_list,
   zbar = z.copy()
 
   for i in range(niter):
-    ss = slice(np.random.randint(0,2),None,2)
+    iss = np.random.randint(0,2)
+    ss  = slice(iss,None,2)
 
     x = np.clip(x - T*zbar, 0, None)
     if verbose: print(i, ss, x)
 
-    y_plus = y[ss] + S[ss]*(fwd_lm(x, sens, events[ss]) + contam_list[ss])
+    y_plus = y[ss] + S[iss]*(fwd_lm(x, sens, events[ss]) + contam_list[ss])
     
     # apply the prox for the dual of the poisson logL
-    y_plus = 0.5*(y_plus + 1 - np.sqrt((y_plus - 1)**2 + 4*S[ss]*mu[ss]))
+    y_plus = 0.5*(y_plus + 1 - np.sqrt((y_plus - 1)**2 + 4*S[iss]*mu[ss]))
     
     dz = back_lm((y_plus - y[ss])/mu[ss], sens, events[ss])
     
@@ -140,8 +142,6 @@ def SPDHG_LM(sens, events, mu, contam_list,
 
   return x
 
-
-
 #---------------------------------------------------------------------------------------------
 
 sens   = np.array([1,0.5])
@@ -151,15 +151,14 @@ contam = np.array([0.02,0.01])
 xtrue = 1*1.2
 ytrue = fwd(xtrue, sens) + contam
 
-seeds = np.arange(32)
-# 31 intereting case where b = [0,1]
+seeds   = np.arange(32)
+precond = False
 
-xML       = np.zeros(seeds.shape[0])
-xMLEM     = np.zeros(seeds.shape[0])
-xSPDHG    = np.zeros(seeds.shape[0])
-xSPDHG_LM = np.zeros(seeds.shape[0])
+xML       = []
+xSPDHG    = []
+xSPDHG_LM = []
 
-niter = 200
+niter = 500
 
 for i, seed in enumerate(seeds):
   np.random.seed(seed)
@@ -167,26 +166,32 @@ for i, seed in enumerate(seeds):
   # generate noise realization (histogram)
   b = np.random.poisson(ytrue)
   print(b)
-  
-  # generate LM events stream
-  events = np.zeros(b.sum(), dtype = np.uint8)
-  events[b[0]:] = 1 
 
-  # shuffle the LM events
-  tmp = np.arange(events.shape[0])
-  np.random.shuffle(tmp)
-  events = events[tmp]
+  # we need >= 2 counts in order to have 2 subsets in LM
+  if b.sum() > 1:
+    # analystic ML solution
+    xML.append(ML(sens, b, contam))
+    xSPDHG.append(SPDHG(sens, b, contam, niter = niter, gamma = 1/xtrue,
+                        verbose = False, precond = precond))
+ 
+    # generate LM events stream
+    events = np.zeros(b.sum(), dtype = np.uint8)
+    events[b[0]:] = 1 
 
-  # counts for each LOR in LM stream
-  mu = b[events]
+    # shuffle the LM events
+    tmp = np.arange(events.shape[0])
+    np.random.shuffle(tmp)
+    events = events[tmp]
 
-  # analystic ML solution
-  xML[i]   = ML(sens, b, contam)
+    # counts for each LOR in LM stream
+    mu = b[events]
 
-  xSPDHG[i]    = SPDHG(sens, b, contam, niter = niter, gamma = 1/xtrue,
-                       verbose = False, precond = True)
-  xSPDHG_LM[i] = SPDHG_LM(sens, events, mu, contam[events], niter = niter, gamma = 1/xtrue, 
-                          verbose = False, precond = True)
+    xSPDHG_LM.append(SPDHG_LM(sens, events, mu, contam[events], niter = niter, gamma = 1/xtrue, 
+                              verbose = False, precond = precond))
+
+xML       = np.array(xML)
+xSPDHG    = np.array(xSPDHG)
+xSPDHG_LM = np.array(xSPDHG_LM)
 
 xML2 = np.clip(xML,0,None)
 
@@ -194,7 +199,7 @@ print('SPDHG    max diff vs non-neg ML', np.abs(xSPDHG - xML2).max())
 print('SPDHG_LM max diff vs non-neg ML', np.abs(xSPDHG_LM - xML2).max())
 
 fig, ax = plt.subplots(figsize = (5,5))
-ax.plot([0,xML2.max()],[0,xML2.max()],'-')
+ax.plot([xML2.min(),xML2.max()],[xML2.min(),xML2.max()],'-')
 ax.plot(xML2,xSPDHG, 'x', label = 'SPDHG')
 ax.plot(xML2,xSPDHG_LM, '.', label = 'LM SPDHG')
 ax.grid(ls = ':')
