86254ad9d8c30e03438866940321848d146479ae
Georg Schramm <georg.schramm@kuleuven.be>
2021-05-03 20:21:02 +0000

    cosmetics

--- python/tof_pet_lm_spdhg.py
+++ python/tof_pet_lm_spdhg.py
@@ -1,8 +1,5 @@
-#TODO: - separate function for LM SPDHG
-#      - prox for dual of L1 and L2 form
-#      - no precond
-
 # small demo for sinogram TOF OS-MLEM
+#TODO: - prox for dual of L1 and L2 form
 
 import os
 import matplotlib.pyplot as plt
@@ -126,33 +123,7 @@ else:
 
   em_sino = img_fwd + contam_sino
 
-#-------------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------------
-
-# create a listmode projector for the LM MLEM iterations
-lmproj = ppp.LMProjector(proj.scanner, proj.img_dim, voxsize = proj.voxsize, 
-                         img_origin = proj.img_origin, ngpus = proj.ngpus,
-                         tof = proj.tof, sigma_tof = proj.sigma_tof, tofbin_width = proj.tofbin_width,
-                         n_sigmas = proj.nsigmas)
-
-# generate list mode events and the corresponting values in the contamination and sensitivity
-# for every subset sinogram
-
-# events is a list of nsubsets containing (nevents,6) arrays where
-# [:,0:2] are the transaxial/axial crystal index of the 1st detector
-# [:,2:4] are the transaxial/axial crystal index of the 2nd detector
-# [:,4]   are the event TOF bins
-# [:,5]   are the events counts (mu_e)
-
-events, multi_index = sino_params.sinogram_to_listmode(em_sino, 
-                                                       return_multi_index = True,
-                                                       return_counts = True)
-
-
 #-----------------------------------------------------------------------------------------------------
-#-----------------------------------------------------------------------------------------------------
-#-----------------------------------------------------------------------------------------------------
-
 def calc_cost(x):
   cost = 0
 
@@ -185,15 +156,70 @@ def _cb(x, **kwargs):
   if 'cost' in kwargs:
     kwargs['cost'][it-1] = calc_cost(x)
 
+  if 'psnr' in kwargs:
+    MSE = ((x - kwargs['xref'])**2).mean()
+    kwargs['psnr'][it-1] = 20*np.log10(kwargs['xref'].max()/np.sqrt(MSE))
+
+
+#-------------------------------------------------------------------------------------
+# rerfence reconstruction using PDHG
+
+niter_ref = 3000
+
+ref_fname = os.path.join('data', f'PDHG_{phantom}_TVbeta_{beta:.1E}_niter_{niter_ref}_counts_{counts:.1E}_seed_{seed}.npz')
+if os.path.exists(ref_fname):
+  tmp = np.load(ref_fname)
+  ref_recon = tmp['ref_recon']
+  cost_ref  = tmp['cost_ref']
+else:
+  cost_ref  = np.zeros(niter_ref)
+  
+  ns = proj.nsubsets
+  proj.init_subsets(1)
+  ref_recon = spdhg(em_sino, attn_sino, sens_sino, contam_sino, proj, niter_ref,
+                    gamma = 1/img.max(), fwhm = fwhm, verbose = True, 
+                    beta = beta, callback = _cb, callback_kwargs = {'cost': cost_ref})
+  proj.init_subsets(ns)
+
+  np.savez(ref_fname, ref_recon = ref_recon, cost_ref = cost_ref)
+
+
+#-------------------------------------------------------------------------------------
+
+# create a listmode projector for the LM MLEM iterations
+lmproj = ppp.LMProjector(proj.scanner, proj.img_dim, voxsize = proj.voxsize, 
+                         img_origin = proj.img_origin, ngpus = proj.ngpus,
+                         tof = proj.tof, sigma_tof = proj.sigma_tof, tofbin_width = proj.tofbin_width,
+                         n_sigmas = proj.nsigmas)
+
+# generate list mode events and the corresponting values in the contamination and sensitivity
+# for every subset sinogram
+
+# events is a list of nsubsets containing (nevents,6) arrays where
+# [:,0:2] are the transaxial/axial crystal index of the 1st detector
+# [:,2:4] are the transaxial/axial crystal index of the 2nd detector
+# [:,4]   are the event TOF bins
+# [:,5]   are the events counts (mu_e)
+
+events, multi_index = sino_params.sinogram_to_listmode(em_sino, 
+                                                       return_multi_index = True,
+                                                       return_counts = True)
+
+
 #-----------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------
 
+base_str = f'{phantom}_counts_{counts:.1E}_beta_{beta:.1E}_niter_{niter_ref}_{niter}_nsub_{nsubsets}'
+
 rho    = 0.999
-gammas = np.array([0.1,0.3,1,3,10]) / img.max()
+gammas = np.array([0.1,0.3,1,3,10,30]) / img.max()
 
 cost_spdhg_sino = np.zeros((len(gammas),niter))
 cost_spdhg_lm   = np.zeros((len(gammas),niter))
+
+psnr_spdhg_sino = np.zeros((len(gammas),niter))
+psnr_spdhg_lm   = np.zeros((len(gammas),niter))
 
 x_sino = np.zeros((len(gammas),) + img.shape)
 x_lm   = np.zeros((len(gammas),) + img.shape)
@@ -201,7 +227,7 @@ x_lm   = np.zeros((len(gammas),) + img.shape)
 for ig,gamma in enumerate(gammas):
   # sinogram SPDHG
   proj.init_subsets(nsubsets)
-  cbs = {'cost':cost_spdhg_sino[ig,:]}
+  cbs = {'cost':cost_spdhg_sino[ig,:],'psnr':psnr_spdhg_sino[ig,:],'xref':ref_recon}
   x_sino[ig,...] = spdhg(em_sino, attn_sino, sens_sino, contam_sino, proj, niter,
                          fwhm = fwhm, gamma = gamma, rho = rho, verbose = True, 
                          beta = beta, callback = _cb, callback_kwargs = cbs)
@@ -209,43 +235,76 @@ for ig,gamma in enumerate(gammas):
 
 
   # LM SPDHG
-  cblm = {'cost':cost_spdhg_lm[ig,:]}
+  cblm = {'cost':cost_spdhg_lm[ig,:],'psnr':psnr_spdhg_lm[ig,:],'xref':ref_recon}
   x_lm[ig,...] = spdhg_lm(events, multi_index,
                           em_sino, attn_sino, sens_sino, contam_sino, 
                           proj, lmproj, niter, nsubsets,
-                          fwhm = fwhm, gamma = 10*gamma, rho = rho, verbose = True, 
+                          fwhm = fwhm, gamma = gamma, rho = rho, verbose = True, 
                           beta = beta, callback = _cb, callback_kwargs = cblm)
 #-----------------------------------------------------------------------------------------------------
-vmax = 1.2*img.max()
-sigs = 4.5/(2.35*voxsize)
+# save the results
+ofile = os.path.join('data',f'{base_str}.npz')
+np.savez(ofile, ref_recon = ref_recon, cost_ref = cost_ref,
+                cost_spdhg_sino = cost_spdhg_sino, psnr_spdhg_sino = psnr_spdhg_sino, 
+                cost_spdhg_lm   = cost_spdhg_lm, psnr_spdhg_lm = psnr_spdhg_lm, 
+                x_sino = x_sino, x_lm = x_lm, gammas = gammas)
 
-fig, ax = plt.subplots(2,len(gammas), figsize = (4*len(gammas),8))
+
+# show the results
+
+vmax = 1.2*img.max()
+
+fig, ax = plt.subplots(2,len(gammas)+1, figsize = (3*(len(gammas)+1),6))
 for i,gam in enumerate(gammas):
   ax[0,i].imshow(x_sino[i,...].squeeze(), vmin = 0, vmax = vmax, cmap = plt.cm.Greys)
   ax[1,i].imshow(x_lm[i,...].squeeze(),   vmin = 0, vmax = vmax, cmap = plt.cm.Greys)
 
-  ax[0,i].set_title(f'SPDHG {gam:.1e}', fontsize = 'medium')
-  ax[1,i].set_title(f'LM SPDHG {gam:.1e}', fontsize = 'medium')
+  ax[0,i].set_title(f'SPDHG {gam:.1e}', fontsize = 'medium', color = plt.get_cmap("tab10")(i))
+  ax[1,i].set_title(f'LM SPDHG {gam:.1e}', fontsize = 'medium', color = plt.get_cmap("tab10")(i))
+
+ax[0,-1].imshow(ref_recon.squeeze(), vmin = 0, vmax = vmax, cmap = plt.cm.Greys)
+ax[1,-1].imshow(img.squeeze(),   vmin = 0, vmax = vmax, cmap = plt.cm.Greys)
+ax[0,-1].set_title(f'reference PDHG', fontsize = 'medium')
+ax[1,-1].set_title(f'ground truth', fontsize = 'medium')
 
 for axx in ax.ravel():
   axx.set_axis_off()
 
 fig.tight_layout()
+fig.savefig(os.path.join('data',f'{base_str}.png'))
 fig.show()
 
-c_ref = min(cost_spdhg_sino.min(), cost_spdhg_lm.min())
+
+
+c_ref = cost_ref.min()
 c_0   = calc_cost(np.zeros(img.shape, dtype = np.float32))
 n     = c_0 - c_ref
 ni    = np.arange(niter) + 1
 
-fig2, ax2 = plt.subplots()
+fig2, ax2 = plt.subplots(1,2, figsize = (10,5))
 for ig, gam in enumerate(gammas):
   col = plt.get_cmap("tab10")(ig)
-  ax2.semilogy(ni, (cost_spdhg_sino[ig,:] - c_ref)/n, 
-               label = f'SPDHG SINO {gam:.2e}', ls = '-', color = col)
-  ax2.semilogy(ni, (cost_spdhg_lm[ig,:] - c_ref)/n, 
-               label = f'SPDHG LM {gam:.2e}', ls = ':', color = col)
-ax2.grid(ls = ':')
-ax2.legend()
+  ax2[0].semilogy(ni, (cost_spdhg_sino[ig,:] - c_ref)/n, '-.', ms = 5,
+               label = f'SINO {gam:.2e}',color = col)
+  ax2[0].semilogy(ni, (cost_spdhg_lm[ig,:] - c_ref)/n, '-v', ms = 5, 
+               label = f'LM {gam:.2e}', color = col)
+  ax2[1].plot(ni, psnr_spdhg_sino[ig,:], '-.', ms = 5, color = col)
+  ax2[1].plot(ni, psnr_spdhg_lm[ig,:], '-v', ms = 5, color = col)
+
+ax2[0].grid(ls = ':')
+ax2[0].set_xlabel('iteration')
+ax2[0].set_ylabel('relative cost')
+ax2[1].grid(ls = ':')
+ax2[1].set_xlabel('iteration')
+ax2[1].set_ylabel('PSNR to reference')
+
+handles, labels = ax2[0].get_legend_handles_labels()
+fig2.legend(handles, labels, loc='upper center', ncol = len(gammas), fontsize = 'small')
+
 fig2.tight_layout()
+fig2.savefig(os.path.join('data',f'{base_str}_metrics.pdf'))
+fig2.savefig(os.path.join('data',f'{base_str}_metrics.png'))
 fig2.show()
+
+
+
