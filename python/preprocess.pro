47814d04662801fda0f0b241e66c51b02e6177ac
Georg Schramm <georg.schramm@kuleuven.be>
2022-04-13 17:17:10 +0200

    update script for preprocessing / reconstruction of real data

--- python/preprocess.pro
+++ python/preprocess.pro
@@ -1,36 +1,6 @@
 ;TODO create dummy event list that contains every geometrical LOR exactly once
 ;     and get corresponding sens/attn sino indices + values for sens image calc.
 
-projd = nidef_proj(/pet3d_dmi, /raytracer, /list, /binmash, /true_segmentsampling)
-
-
-listfile = '/uz/data/Admin/ngeworkingresearch/mi_patient_data/respmotion/20210930_16918_61360708/LST/LIST0006.BLF'
-listdescrip = {scanner:      'dmi',       $
-               acquisition:  'listmode',  $
-               listfile:      listfile }
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-events = niread_hdf5(listdescrip.listfile, 'MiceList', 'TofCoinc', /list)
-events = events[[1,0,3,2,4],*]
-               
-tofbin_orig = events[4,*] ; could get flipped
-tofbin = tofbin_orig
-dummy = nipet3dcoords(projd.petstruct, /verbose, $
-                      idx1 = events[0,*], ring1 = events[1,*], $
-                      idx2 = events[2,*], ring2 = events[3,*], $
-                      x1, y1, z1, x2, y2, z2, $
-                      radial = radial, angle = angle, plane = plane, bin = tofbin)
-
-; mesh and shift the original tofbins 
-tofbin_orig = (tofbin_orig / 13) + 14
-
-tofbin      = (projd.nrbins - 1) - tofbin      ; difference in definitions!
-tofbin_orig = (projd.nrbins - 1) - tofbin_orig ; difference in definitions!
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 sinofile = '/uz/data/Admin/ngeworkingresearch/mi_patient_data/respmotion/20210930_16918_61360708/raw_idlfiles/rdf.7.1'
 sinodescrip = { scanner:            'dmi',      $
                 emission:           sinofile,   $
@@ -40,11 +10,51 @@ sinodescrip = { scanner:            'dmi',      $
                 scatter_ds_scatter: sinofile,   $
                 randoms:            sinofile}
 
+listfile = '/uz/data/Admin/ngeworkingresearch/mi_patient_data/respmotion/20210930_16918_61360708/LST/LIST0006.BLF'
+listdescrip = {scanner:      'dmi',       $
+               acquisition:  'listmode',  $
+               listfile:      listfile }
+
+; define a projector using the "real" geometry (no LOR meshing)
+projd = nidef_proj(/pet3d_dmi, /raytracer, /list, /binmash, /true_segmentsampling)
+
+
+
+; read the corrections sinograms
+dummy = niread_data(sinodescrip, projd=projd, /noemis, sens=sens, atten=atten, contam=contam, /verbose)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; generate pseudo attenuation and sens sinograms that we need to caluclate the sensitivity image
+;
+coords_all = nipet3dcoords(projd.petstruct, /allxtals)
+; get the sensitivity and attenuation values for all LORs in LM format
+; we need those to calculate the sensivity image
+sens_all  = nipet3dcoords(projd.petstruct, /samplesinogram, sinogram=sens)
+atten_all = nipet3dcoords(projd.petstruct, /samplesinogram, sinogram=atten)
+
+stop
+
+print, 'sens'
+niwrite_hdf5, sens_all, "data/dmi/lm_data.h5", "all_xtals", "sens"
+print, 'atten'
+niwrite_hdf5, atten_all, "data/dmi/lm_data.h5", "all_xtals", "atten"
+print, 'xtals'
+niwrite_hdf5, coords_all, "data/dmi/lm_data.h5", "all_xtals", "xtal_ids"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+print, 'reading LM'
 coords = niread_data(listdescrip, projd=projd, indices=indices, /verbose)
 
-dummy = niread_data(sinodescrip, projd=projd, /noemis, sens=sens, atten=atten, contam=contam, /verbose)
-;if n_elements(sens) gt 1 then sensvalues = reform(sens[indices[0,*],indices[1,*],indices[2,*]])
-;if n_elements(atten) gt 1 then attenvalues = reform(atten[indices[0,*],indices[1,*],indices[2,*]])
-;if n_elements(contam) gt 1 then contamvalues = reform(contam[indices[0,*],indices[1,*],indices[2,*],coords[-1,*]])
+if n_elements(sens) gt 1 then sensvalues = reform(sens[indices[0,*],indices[1,*],indices[2,*]])
+if n_elements(atten) gt 1 then attenvalues = reform(atten[indices[0,*],indices[1,*],indices[2,*]])
+if n_elements(contam) gt 1 then contamvalues = reform(contam[indices[0,*],indices[1,*],indices[2,*],coords[-1,*]])
+
+niwrite_hdf5, sensvalues,   "data/dmi/lm_data.h5", "correction_lists", "sens"
+niwrite_hdf5, attenvalues,  "data/dmi/lm_data.h5", "correction_lists", "atten"
+niwrite_hdf5, contamvalues, "data/dmi/lm_data.h5", "correction_lists", "contam"
+
+niwrite_hdf5, sinofile, "data/dmi/lm_data.h5", "header", "sinofile"
+niwrite_hdf5, listfile, "data/dmi/lm_data.h5", "header", "listfile"
 
 END
--- python/tof_pet_lm_spdhg_dmi.py
+++ python/tof_pet_lm_spdhg_dmi.py
@@ -1,24 +1,46 @@
 import pyparallelproj as ppp
-import numpy as np
+from pyparallelproj.utils import GradientOperator, GradientNorm
+from pyparallelproj.algorithms import spdhg, osem_lm_emtv
+from pyparallelproj.models import pet_fwd_model_lm, pet_back_model_lm
+from algorithms import spdhg_lm
 import h5py
+import numpy as np
+import matplotlib.pyplot as plt
 
 from pathlib import Path
 from utils import count_event_multiplicity
 
-#---------------------------------------------------------------------
+def _cb(x, **kwargs):
+  it = kwargs.get('iteration',0)
+  np.save(f'data/dmi/it_{it:03}.npy',x)
 
-with open('.pdir', 'r') as f:
-  pdir = Path(f.read().strip())
+#--------------------------------------------------------------------------------------------
 
 voxsize   = np.array([2.78, 2.78, 2.78], dtype = np.float32)
-img_shape = (200,200,71)
+img_shape = (120,120,71)
 verbose   = True
 
-#---------------------------------------------------------------------
-#---------------------------------------------------------------------
-#---------------------------------------------------------------------
+# FHHM for resolution model in voxel
+fwhm  = 4.5 / (2.35*voxsize)
+
+# prior strength
+beta = 3e1
 
 np.random.seed(1)
+
+#--------------------------------------------------------------------------------------------
+# setup scanner and projector
+
+# define the 4 ring DMI geometry
+scanner = ppp.RegularPolygonPETScanner(
+               R                    = 0.5*(744.1 + 2*8.51),
+               ncrystals_per_module = np.array([16,9]),
+               crystal_size         = np.array([4.03125,5.31556]),
+               nmodules             = np.array([34,4]),
+               module_gap_axial     = 2.8)
+
+
+# define sinogram parameter - also needed to setup the LM projector
 
 # speed of light in mm/ns
 speed_of_light = 300.
@@ -29,17 +51,6 @@ time_res_FWHM = 0.385
 # sigma TOF in mm
 sigma_tof = (speed_of_light/2) * (time_res_FWHM/2.355)
 
-# define the 4 ring DMI geometry
-scanner = ppp.RegularPolygonPETScanner(
-               R                    = 0.5*(744.1 + 2*8.51),
-               ncrystals_per_module = np.array([16,9]),
-               crystal_size         = np.array([4.03125,5.31556]),
-               nmodules             = np.array([34,4]),
-               module_gap_axial     = 2.8)
-
-#scanner.show_crystal_config()
-
-
 # the TOF bin width in mm is 13*0.01302ns times the speed of light (300mm/ns) divided by two
 sino_params = ppp.PETSinogramParameters(scanner, rtrim = 65, ntofbins = 29, 
                                         tofbin_width = 13*0.01302*speed_of_light/2)
@@ -49,14 +60,49 @@ proj = ppp.SinogramProjector(scanner, sino_params, img_shape,
                              voxsize = voxsize, tof = True, 
                              sigma_tof = sigma_tof, n_sigmas = 3.)
 
-#---------------------------------------------------------------------
-#---------------------------------------------------------------------
-#---------------------------------------------------------------------
 
-if verbose: print('Reading LM data')
+#--------------------------------------------------------------------------------------------
+# calculate sensitivity image
+
+if verbose:
+  print('Reading sens / atten "sinogram"')
+
+with h5py.File('data/dmi/lm_data.h5', 'r') as data:
+  all_possible_events = data['all_xtals/xtal_ids'][:]
+  all_sens  =  data['all_xtals/sens'][:]
+  all_atten =  data['all_xtals/atten'][:]
+
+# calculate the sensivity image
+# TODO: use TOF backprojector for sens_img calculation
+if verbose:
+  print('Calculating sensitivity image')
+proj.set_tof(False)
+ones = np.ones(all_possible_events.shape[0], dtype = np.float32)
+sens_img = pet_back_model_lm(ones, proj, all_possible_events, 
+                             all_atten, all_sens, fwhm = fwhm) 
+sens_img = proj.back_project_lm(all_sens*all_atten, all_possible_events)
+proj.set_tof(True)
+
+
+# delete all event variables that are not needed any more
+del ones
+del all_possible_events
+del all_sens
+del all_atten
+
+#--------------------------------------------------------------------------------------------
+
+# read the actual LM data and the correction lists
 
 # read the LM data
-LM_file = pdir / 'LST' / 'LIST0006.BLF'
+if verbose:
+  print('Reading LM data')
+
+with h5py.File('data/dmi/lm_data.h5', 'r') as data:
+  sens_list   =  data['correction_lists/sens'][:]
+  atten_list  =  data['correction_lists/atten'][:]
+  contam_list =  data['correction_lists/contam'][:]
+  LM_file     =  Path(data['header/listfile'][0].decode("utf-8"))
 
 with h5py.File(LM_file, 'r') as data:
   events = data['MiceList/TofCoinc'][:]
@@ -72,21 +118,42 @@ events[:,-1] = -(events[:,-1]//13)
 
 nevents = events.shape[0]
 
-## shuffle events since events come semi sorted
-#if verbose: print('shuffling LM data')
+### shuffle events since events come semi sorted
+#if verbose: 
+#  print('shuffling LM data')
 #ie = np.arange(nevents)
 #np.random.shuffle(ie)
 #events = events[ie,:]
-#
-## calculate the events multiplicity
-#mu = count_event_multiplicity(events, use_gpu_if_possible = True)
-#
-##---------------------------------------------------------------------
-##---------------------------------------------------------------------
-##---------------------------------------------------------------------
-#
-## backproject all LM events
-#if verbose: print('backprojecting LM data')
-#bimg = proj.back_project_lm(np.ones(nevents, dtype = np.float32), events)
-#import pymirc.viewer as pv
-#vi = pv.ThreeAxisViewer(bimg)
+#sens_list   = sens_list[ie]
+#atten_list  = atten_list[ie]  
+#contam_list = contam_list[ie]
+
+# calculate the events multiplicity
+if verbose:
+  print('Calculating event count')
+mu = count_event_multiplicity(events, use_gpu_if_possible = True)
+
+# back project ones
+if verbose:
+  print('backprojecting LM data')
+bimg = proj.back_project_lm(np.ones(nevents, dtype = np.float32), events)
+
+
+#--------------------------------------------------------------------------------------------
+grad_operator = GradientOperator()
+grad_norm     = GradientNorm(name = 'l2_l1')
+
+# run EM-TV
+if verbose:
+  print('running OSEM')
+xinit = osem_lm_emtv(events, atten_list, sens_list, contam_list, proj, sens_img, 1, 28, 
+                     grad_operator = grad_operator, grad_norm = grad_norm,
+                     fwhm = fwhm, verbose = True, beta = beta)
+
+norm = gaussian_filter(xinit.squeeze(),3).max()
+
+xspdhg = spdhg_lm(events, atten_list, sens_list, contam_list, sens_img,
+                  proj, 10, 56, x0 = xinit,
+                  fwhm = fwhm, gamma = 30 / norm, verbose = True, rho = 8,
+                  callback = _cb,
+                  grad_operator = grad_operator, grad_norm = grad_norm, beta = beta)
